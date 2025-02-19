4ba8e7bf95fbef529669325167e0daf6a9fb55bb
Anshumaan Soni <102132714+TETRAWasTaken@users.noreply.github.com>
2025-02-19 10:18:12 +0530

    Date 15 Jan 2025, Programming in C lab

--- stringlooping.c
+++ stringlooping.c
@@ -4,10 +4,65 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+char vowels[10]={'a','e','i','o','u','A','E','I','O','U'};
+
+void len(char t[]) {
+    int len=0,i=0;
+    while (t[i]!='\0') {
+        len++;
+        i++;
+    }
+    printf("The length of the string is: %d",len);
+}
+
+void charac(char t[]) {
+    int len=0,i=0;
+    while (t[i]!='\0') {
+        if (t[i]==' ' || t[i]=='\n') {
+            continue;
+        }
+        else {
+            len++;
+        }
+    }
+    printf("No. of characters are : %d",len);
+}
+
+int check(char n) {
+    int temp;
+    for (int i=0;i<=9;i++) {
+        if (n==vowels[i]) {
+            temp++;
+        }
+        else {
+            continue;
+        }
+        return temp;
+    }
+}
+
+void vowel(char t[]) {
+    int len=0,i=0;
+    while (t[i]!='\0') {
+        if (check(t[i])>0) {
+            len++;
+        }
+        else {
+            continue;
+        }
+    }
+    printf("No. of Vowels are : %d",len);
+}
+
 
 void main() {
-    int len =0;
-    char a[12] = "hello";
-    fgets(a,12,stdin);
-    printf("%s",a);
+    char a[20];
+    printf("Enter a string: ");
+    //fgets(a,20,stdin);
+    scanf("%s",a);
+    len(a);
+    charac(a);
+    vowel(a);
 }
