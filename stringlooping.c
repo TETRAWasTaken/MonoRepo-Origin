5247c26c4dc4f7c91774cf0fc67fc44c86cc7934
Anshumaan Soni <102132714+TETRAWasTaken@users.noreply.github.com>
2025-02-19 09:21:31 +0530

    Date 15 Jan 2025, Programming in C lab

---
+++ stringlooping.c
@@ -0,0 +1,13 @@
+//
+// Created by Anshumaan soni on 2/19/25.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+    int len =0;
+    char a[12] = "hello";
+    fgets(a,12,stdin);
+    printf("%s",a);
+}
